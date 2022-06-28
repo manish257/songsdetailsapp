d771f388198815f52e5a1daf0cfd8a33055d841b
manish257 <manishmahato257@gmail.com>
2022-06-28 15:17:08 +0500

    button done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import SongList from './SongList';
+import React from "react";
+import SongList from "./SongList";
 
 const App = () => {
-    return(
-        <div>
-            <SongList />
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default App;

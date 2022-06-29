4690c4edf8f2199623698a2b629bbe9532102e9a
manish257 <manishmahato257@gmail.com>
2022-06-29 11:32:07 +0500

    details complete

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-    console.log(props);
+const SongDetail = ({ song }) => {
+    //console.log(props);
+
+    if (!song){
+        return <div>Select a song</div>
+    }
+
     return ( 
         <div>
-            Song Detail
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
         </div>
      );
 }
